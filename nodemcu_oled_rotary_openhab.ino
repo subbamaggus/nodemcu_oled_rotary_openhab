0b93099fb31df5d04480e3517c4a23dfe67c42b4
Markus Kerle <m.kerle@jeanmueller.de>
2023-04-12 16:21:02 +0200

    adjust example to work with my setup

---
+++ nodemcu_oled_rotary_openhab.ino
@@ -0,0 +1,81 @@
+#include "AiEsp32RotaryEncoder.h"
+#include "Arduino.h"
+
+// base version from:
+// https://github.com/igorantolic/ai-esp32-rotary-encoder.git
+
+#define ROTARY_ENCODER_A_PIN 12
+#define ROTARY_ENCODER_B_PIN 14
+#define ROTARY_ENCODER_BUTTON_PIN 13
+
+#define ROTARY_ENCODER_VCC_PIN -1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+
+//depending on your encoder - try 1,2 or 4 to get expected behaviour
+#define ROTARY_ENCODER_STEPS 4
+
+//instead of changing here, rather change numbers above
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+
+void rotary_onButtonClick()
+{
+	static unsigned long lastTimePressed = 0;
+	//ignore multiple press in that time milliseconds
+	if (millis() - lastTimePressed < 500)
+	{
+		return;
+	}
+	lastTimePressed = millis();
+	Serial.print("button pressed ");
+	Serial.print(millis());
+	Serial.println(" milliseconds after restart");
+}
+
+void rotary_loop()
+{
+	//dont print anything unless value changed
+	if (rotaryEncoder.encoderChanged())
+	{
+		Serial.print("Value: ");
+		Serial.println(rotaryEncoder.readEncoder());
+	}
+	if (rotaryEncoder.isEncoderButtonClicked())
+	{
+		rotary_onButtonClick();
+	}
+}
+
+void IRAM_ATTR readEncoderISR()
+{
+	rotaryEncoder.readEncoder_ISR();
+}
+
+void setup()
+{
+	Serial.begin(9600);
+
+	//we must initialize rotary encoder
+	rotaryEncoder.begin();
+	rotaryEncoder.setup(readEncoderISR);
+	//set boundaries and if values should cycle or not
+	//in this example we will set possible values between 0 and 1000;
+	bool circleValues = false;
+	rotaryEncoder.setBoundaries(0, 1000, circleValues); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+
+	/*Rotary acceleration introduced 25.2.2021.
+   * in case range to select is huge, for example - select a value between 0 and 1000 and we want 785
+   * without accelerateion you need long time to get to that number
+   * Using acceleration, faster you turn, faster will the value raise.
+   * For fine tuning slow down.
+   */
+	//rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
+	rotaryEncoder.setAcceleration(250); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
+
+  Serial.println("setup done");
+}
+
+void loop()
+{
+	//in loop call your custom function which will process rotary encoder values
+	rotary_loop();
+	delay(50); //or do whatever you need to do...
+}
